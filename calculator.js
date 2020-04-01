0ca61b3e3105afde72fa8deeebfe012e38985ed3
SuelyBarreto <suely@barreto.us>
2020-03-31 20:02:48 -0700

    added prompt schema

--- calculator.js
+++ calculator.js
@@ -66,8 +66,49 @@ const calculate = function(userInput) {
 const calculateUserInput = function(error, promptInput) {
   calculate(promptInput);
   console.log(`\nThanks for using the calculator!`);
-}  
-prompt.get(['num1','num2','operation'], calculateUserInput);
+} 
+
+const schema = {
+  properties: {
+    num1: {
+      description: 'Enter first number',
+      type: 'integer',
+      message: 'Please enter a number',
+      required: true
+    },
+    num2: {
+      description: 'Enter second number',
+      type: 'integer',
+      message: 'Please enter a number',
+      required: true
+    },
+    operation: {
+      description: 'Enter operation',
+      type: 'string',
+      message: 'Please enter a number',
+      required: true
+    }
+  }
+};
+
+console.log(`Welcome to the Calculator!`);
+console.log(`Enter two numbers and an operation.`);
+console.log(`Valid operations are: add (+), subtract (-), multiply (*), divide (/), exponent (^) and modulo (%).`);
+prompt.get(schema, calculateUserInput);
+
+
+// {
+//   description: 'Enter your password',     // Prompt displayed to the user. If not supplied name will be used.
+//   type: 'string',                 // Specify the type of input to expect.
+//   pattern: /^\w+$/,                  // Regular expression that input must be valid against.
+//   pattern: /^[0-9]+$/,
+//   message: 'Password must be letters', // Warning message to display if validation fails.
+//   hidden: true,                        // If true, characters entered will either not be output to console or will be outputed using the `replace` string.
+//   replace: '*',                        // If `hidden` is set it will replace each hidden character with the specified string.
+//   default: 'lamepassword',             // Default value to use if no value is entered.
+//   required: true                        // If true, value entered must be non-empty.
+//   before: function(value) { return 'v' + value; } // Runs before node-prompt callbacks. It modifies user's input
+// }
 
 
 // Questions to ask and answer:
